fcedea83fe906a3362003052b090671bc70f48df
valart <artjom.valdas@ut.ee>
2023-02-12 22:04:11 +0200

    Making nodes work together (env and readCSV)

--- nodetypes/mltosca.nodes.MLBlock.Data.Reading/Csv/files/ReadCSV/ReadCSV.py
+++ nodetypes/mltosca.nodes.MLBlock.Data.Reading/Csv/files/ReadCSV/ReadCSV.py
@@ -4,15 +4,14 @@ from pandas import DataFrame
 
 
 def read_csv(filepath) -> DataFrame:
-  return pd.read_csv(filepath)
+    return pd.read_csv(filepath)
 
 
 def save_dataframe(df: DataFrame):
-  df.to_pickle('df.pkl')
+    df.to_pickle('/home/artjom/Downloads/df.pkl')
 
 
 if __name__ == "__main__":
-  csvPath = sys.argv[1]
-  df = read_csv(csvPath)
-  save_dataframe(df)
-
+    csvPath = sys.argv[1]
+    df = read_csv(csvPath)
+    save_dataframe(df)
