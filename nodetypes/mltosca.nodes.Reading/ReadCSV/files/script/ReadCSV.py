f79c38c076adda47103b3585e7e3a06c074864f2
valart <artjom.valdas@ut.ee>
2023-02-28 16:31:33 +0200

    Adding pipeline, splitting and sklearn model

--- nodetypes/mltosca.nodes.Reading/ReadCSV/files/script/ReadCSV.py
+++ nodetypes/mltosca.nodes.Reading/ReadCSV/files/script/ReadCSV.py
@@ -8,16 +8,14 @@ def read_csv(filepath, delimiter=',', encoding='UTF-8') -> DataFrame:
     return pd.read_csv(filepath, delimiter=delimiter, encoding=encoding)
 
 
-def save_dataframe(df: DataFrame):
-    path = os.path.expanduser('~') + '/mltosca'
-    if not os.path.exists(path):
-        os.mkdir(path)
-    df.to_pickle(path + '/df.pkl')
+def save_dataframe(df: DataFrame, project_location: str):
+    df.to_pickle(project_location + '/processing.pkl')
+    os.rename(project_location + '/processing.pkl', project_location + '/df.pkl')
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         sys.exit("Not enough args")
-    filepath, delimiter, encoding = sys.argv[1], sys.argv[2], sys.argv[3]
+    project_location, filepath, delimiter, encoding = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
     df = read_csv(filepath, delimiter, encoding)
-    save_dataframe(df)
+    save_dataframe(df, project_location)
